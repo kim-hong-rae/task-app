d727c2c5e581bba3bc62409c2eed7331f0ee1118
kimhongrae <khr0690@hanmail.net>
2024-09-03 01:50:22 +0900

    feat : 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./page/Home";
+import GlobalStyle from "./style/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Home />
+    </>
+  );
+}
+
+export default App;
