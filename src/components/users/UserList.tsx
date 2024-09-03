95e435dd7717a56f39d7a5113b2b822551a5a8a3
kimhongrae <khr0690@hanmail.net>
2024-09-04 03:40:02 +0900

    refactor: userlist 컴포넌트 리팩토링

--- src/components/users/UserList.tsx
+++ src/components/users/UserList.tsx
@@ -1,10 +1,10 @@
-// src/components/users/UserList.tsx
-import { useEffect, useState } from "react";
-import { fetchRandomUser } from "../../api/api";
-import { SortType, User } from "../../type/type";
+import { SortType } from "../../type/type";
 import UserItem from "./UserItem";
 import Pagination from "../common/Pagination";
 import styled from "styled-components";
+import { useFetchUsers } from "../../hook/useFetchUsers";
+import { useFilterSortUsers } from "../../hook/useFilterSort";
+import { usePagination } from "../../hook/usePagination";
 
 interface UserListProps {
   searchValue: string;
@@ -12,46 +12,14 @@ interface UserListProps {
 }
 
 const UserList = ({ searchValue, sortOrder }: UserListProps) => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
-  const usersPerPage = 6;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      setLoading(true);
-      try {
-        const response = await fetchRandomUser();
-        const allUsers = response.results;
-        setUsers(allUsers);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    getUserData();
-  }, []);
-
-  const filteredUsers = users.filter((user) =>
-    `${user.name.first} ${user.name.last}`
-      .toLowerCase()
-      .includes(searchValue.toLowerCase())
-  );
-
-  const sortedUsers = [...filteredUsers].sort((a, b) => {
-    const nameA = `${a.name.first} ${a.name.last}`.toLowerCase();
-    const nameB = `${b.name.first} ${b.name.last}`.toLowerCase();
-    return sortOrder === "asc"
-      ? nameA.localeCompare(nameB)
-      : nameB.localeCompare(nameA);
-  });
-
-  const totalPages = Math.ceil(sortedUsers.length / usersPerPage);
-  const indexOfLastUser = currentPage * usersPerPage;
-  const indexOfFirstUser = indexOfLastUser - usersPerPage;
-  const currentUsers = sortedUsers.slice(indexOfFirstUser, indexOfLastUser);
-
+  const { users, loading } = useFetchUsers();
+  const sortedUsers = useFilterSortUsers(users, searchValue, sortOrder);
+  const {
+    currentItems: currentUsers,
+    currentPage,
+    setCurrentPage,
+    totalPages,
+  } = usePagination(sortedUsers, 6);
   return (
     <UserListStyle>
       {loading ? (
