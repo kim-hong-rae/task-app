8b861cca7b6eb487f1c198a5dfee3fbf9f8f792e
kimhongrae <khr0690@hanmail.net>
2024-09-04 03:39:50 +0900

    refactor: usePagination 커스텀 훅 생성

---
+++ src/hook/usePagination.ts
@@ -0,0 +1,15 @@
+import { useState, useMemo } from "react";
+
+export const usePagination = <T>(items: T[], itemsPerPage: number) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(items.length / itemsPerPage);
+
+  const currentItems = useMemo(() => {
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    return items.slice(indexOfFirstItem, indexOfLastItem);
+  }, [items, currentPage, itemsPerPage]);
+
+  return { currentItems, currentPage, setCurrentPage, totalPages };
+};
