01e2b5e602a81dc403241f38bca2a2afdf5335e5
kimhongrae <khr0690@hanmail.net>
2024-09-04 14:46:48 +0900

    feat : gitignore에 env 파일 추가 및 api 변경

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 export const fetchRandomUser = async () => {
   try {
-    const response = await axios.get("https://randomuser.me/api/?results=12");
+    const response = await axios.get(BASE_URL);
     console.log(response.data);
     return response.data;
   } catch (error) {
-    console.error("Error fetching user:", error);
+    console.error("error :", error);
     throw error;
   }
 };
